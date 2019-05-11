3a926c22284610ed6e6979410bf7e731af488cee
s-kris <skris4291@gmail.com>
2019-05-11 19:42:19 +0530

    adjust layout to accomodate post and comments w/ medium bg colors

--- src/components/layout.js
+++ src/components/layout.js
@@ -56,8 +56,6 @@ class Layout extends React.Component {
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(27),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
         <header>{header}</header>
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 import Image from "gatsby-image"
 import readingTime from "reading-time"
 import Disqus from "disqus-react"
@@ -14,87 +14,126 @@ class BlogPostTemplate extends React.Component {
     const post = this.props.data.markdownRemark
     const { avatar } = this.props.data
     const { title, author, disqusShortname } = this.props.data.site.siteMetadata
-
+    const maxWidth = rhythm(27)
     return (
       <Layout location={this.props.location} title={title}>
         <SEO
           title={post.frontmatter.title}
           description={post.frontmatter.description || post.excerpt}
         />
-        <h1
-          style={{
-            ...scale(6 / 5),
-          }}
-        >
-          {post.frontmatter.title}
-        </h1>
         <div
           style={{
-            display: `flex`,
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
           }}
         >
-          <Image
-            fixed={avatar.childImageSharp.fixed}
-            alt={author}
-            style={{
-              marginRight: rhythm(1 / 2),
-              marginBottom: 0,
-              minWidth: 50,
-              borderRadius: `100%`,
-            }}
-            imgStyle={{
-              borderRadius: `50%`,
-            }}
-          />
+          <div style={{ maxWidth: maxWidth }}>
+            <h1
+              style={{
+                ...scale(6 / 5),
+              }}
+            >
+              {post.frontmatter.title}
+            </h1>
+            <div
+              style={{
+                display: `flex`,
+              }}
+            >
+              <Image
+                fixed={avatar.childImageSharp.fixed}
+                alt={author}
+                style={{
+                  marginRight: rhythm(1 / 2),
+                  marginBottom: 0,
+                  minWidth: 50,
+                  borderRadius: `100%`,
+                }}
+                imgStyle={{
+                  borderRadius: `50%`,
+                }}
+              />
 
-          <div>
-            <p
+              <div>
+                <p
+                  style={{
+                    ...scale(-1 / 5),
+                    display: `block`,
+                    marginBottom: rhythm(1),
+                    color: "black",
+                  }}
+                >
+                  {author}
+                </p>
+                <p
+                  style={{
+                    ...scale(-1.5 / 5),
+                    display: `block`,
+                    marginTop: rhythm(-1),
+                    color: "grey",
+                  }}
+                >
+                  {post.frontmatter.date} &#183; {readingTime(post.html).text}
+                </p>
+              </div>
+            </div>
+            <br />
+            <div dangerouslySetInnerHTML={{ __html: post.html }} />
+            <hr
               style={{
-                ...scale(-1 / 5),
-                display: `block`,
                 marginBottom: rhythm(1),
-                color: "black",
               }}
-            >
-              {author}
-            </p>
-            <p
-              style={{
-                ...scale(-1.5 / 5),
-                display: `block`,
-                marginTop: rhythm(-1),
-                color: "grey",
-              }}
-            >
-              {post.frontmatter.date} &#183; {readingTime(post.html).text}
-            </p>
+            />
+            <Bio />
           </div>
         </div>
         <br />
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
-        <Bio />
-        <br />
         <div
           style={{
-            backgroundColor: "#ffffff",
-            padding: rhythm(1),
-            border: "1px solid #E3E3E3",
-            borderRadius: rhythm(1 / 5),
+            backgroundColor: "#fafafa",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            padding: rhythm(2.5),
           }}
         >
-          <Disqus.DiscussionEmbed
-            shortname={disqusShortname}
-            config={{
-              url: this.props.location.href,
-              identifier: this.props.location.pathname,
-              title: post.frontmatter.title,
+          <div
+            style={{
+              display: "flex",
+              width: "100%",
+              flexDirection: "column",
+              maxWidth: maxWidth,
             }}
-          />
+          >
+            <p
+              style={{
+                ...scale(-1 / 5),
+                fontWeight: "bold",
+                color: "grey",
+              }}
+            >
+              Responses
+            </p>
+            <div
+              style={{
+                backgroundColor: "#ffffff",
+                padding: rhythm(1),
+                border: "1px solid #E3E3E3",
+                borderRadius: rhythm(1 / 5),
+              }}
+            >
+              <Disqus.DiscussionEmbed
+                shortname={disqusShortname}
+                config={{
+                  url: this.props.location.href,
+                  identifier: this.props.location.pathname,
+                  title: post.frontmatter.title,
+                }}
+              />
+            </div>
+          </div>
         </div>
       </Layout>
     )
